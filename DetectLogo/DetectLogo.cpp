c7c9560dacf22cd0d485241c82c7655ea5205ddf
SPHINX <sphinx@gmail.com>
2021-01-07 15:31:21 -0800

    Release v2.0

--- DetectLogo/DetectLogo.cpp
+++ DetectLogo/DetectLogo.cpp
@@ -12,16 +12,12 @@
 #include <Windows.h>
 #include <cstdlib>
 
-#define  _MYDEBUG
 using namespace std;
 using namespace cv;
 
 //! [declare]
 /// Global Variables
 Mat img; Mat templ; Mat result; Mat cropImage;
-const char* comp_window = "Compare Image";
-const char* crop_window = "Crop window";
-const char* templ_window = "Templ window";
 
 const int mean_thresold = 10;
 const int group_threshold = 4;
@@ -37,31 +33,15 @@ vector <Match> matchResult;
 /// Function Headers
 bool DetectLogo(char*, char*);
 Point MatchingMethod(int, void*, int);
-void GetEdgeRect(Mat*);
 /**
  * @function main
  */
 int main(int argc, char** argv)
 {
-
-    char pLetter[100];
-    char pLogo[100];
-#ifdef _MYDEBUG
-    for (int i = 1; i <= 18; i++) {
-    	for (int j = 1; j <= 18; j++){
-
-			sprintf_s(pLetter, "images/letter/%d.jpg", i);
-			sprintf_s(pLogo, "images/logo/%d.jpg", j);
-            //if(DetectLogo(pLetter, pLogo) && i != j)
-		        cout << i << " : " << j << " >> " << DetectLogo(pLetter, pLogo) << endl;
-            matchResult.clear();
-    	}
-    }
-#else
-	if (!DetectLogo(argv[1], argv[2]))
-		cout << "Can't find logo in the image" << endl;
-#endif // _MYDEBUG
-
+    if (!DetectLogo(argv[1], argv[2]))
+        cout << "Can't find logo in the image" << endl;
+    else
+        cout << "Found Logo" << endl;
 }
 
 bool DetectLogo(char* _letter, char* _logo){
@@ -82,17 +62,6 @@ bool DetectLogo(char* _letter, char* _logo){
     }
     //! [load_image]
 
-#ifndef _MYDEBUG
-    //! [create_windows]
-    /// Create windows
-    namedWindow(comp_window, WINDOW_AUTOSIZE);
-	namedWindow(crop_window, WINDOW_AUTOSIZE);
-	namedWindow(templ_window, WINDOW_AUTOSIZE);
-	moveWindow(comp_window,  0, 0);
-	moveWindow(crop_window,  500, 0);
-	moveWindow(templ_window, 900, 0);
-    //! [create_windows]
-#endif // _MYDEBUG
     //! [find correct position]
 	for (int i = 0; i <= max_Method; i++)
     {
@@ -112,9 +81,8 @@ bool DetectLogo(char* _letter, char* _logo){
         }
     }
     if (matchResult.size() >= group_threshold)
-    {
         return false;
-    }
+    
     int cor_index = 0;
     int max_pick = 0;
     for (int i = 0; i < matchResult.size(); i++)
@@ -133,47 +101,14 @@ bool DetectLogo(char* _letter, char* _logo){
 	//! [Crop Logo]
     
     //! [Compare Image]
-//     compare(cropImage, templ, compImage, CMP_EQ);
-//     cvtColor(compImage, compImage, COLOR_RGB2GRAY);
-//     int percentage = countNonZero(compImage);
-//     cout << percentage;
-    //! [Compare Image]
 	
-    //! [get edge rect]
-//    binImage = cropImage.clone();
-// 	threshold(cropImage, cropImage, 128, 255, 0);
-// 	threshold(templ, templ, 128, 255, 0);
 	if (abs(mean(cropImage).val[2] - mean(templ).val[2]) 
         + abs(mean(cropImage).val[1] - mean(templ).val[1]) 
         + abs(mean(cropImage).val[0] - mean(templ).val[0]) > mean_thresold)
         return false;
-//     int edgeLeft;
-//     for (int x = 0; x < binImage.rows; x++)
-//     {
-//         for (int y = 0; y < binImage.cols; y++)
-//         {
-//             Vec3b pix = binImage.at<Vec3b>(x, y);
-//             if (pix[0] != pix[1]) {
-//                 edgeLeft = x;
-//             }
-//         }
-//     }
-    //! [get edge rect]
-#ifndef _MYDEBUG
-    //! [imshow]
-	/// Show me what you got
-	rectangle(img, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
-	
-    imshow(comp_window, img);
-	imshow(crop_window, cropImage);
-	imshow(templ_window, templ);
-	//! [imshow]
-    //! [wait_key]
-    waitKey(0);
-#endif // _MYDEBUG
+    //! [Compare Image]
 
     return true;
-    //! [wait_key]
 }
 
 /**
@@ -220,8 +155,4 @@ Point MatchingMethod(int, void*, int match_method)
     }
     //! [match_loc]
     return matchLoc;
-}
-
-void GetEdgeRect(Mat* _src) {
-    threshold(*_src, *_src, 50, 255, 3);
 }
